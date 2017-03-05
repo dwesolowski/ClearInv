c85ee48bc44c326056f9a42da02ab454abb8ac6e
Daren Wesolowski <daren.wesolowski@icloud.com>
2017-03-05 09:33:38 -0500

    initial import

---
+++ README.md
@@ -0,0 +1,9 @@
+# ClearInventory - Plugin for Bukkit | Spigot
+
+Removes all items from a players inventory on join / exit
+
+#### Supported Versions: 1.11+
+
+## Installation
+
+To use this plugin place the .jar and any other files in your plugins directory and restart the server.
---
+++ src/main/java/com/github/dwesolowski/clearinv/ClearInv.java
@@ -0,0 +1,24 @@
+package com.github.dwesolowski.clearinv;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ClearInv extends JavaPlugin implements Listener {
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        e.getPlayer().getInventory().clear();
+    }
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent e) {
+        e.getPlayer().getInventory().clear();
+    }
+}
